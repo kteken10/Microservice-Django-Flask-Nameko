cb273d578e0ad5b24888aebd29714e0b19dd3b49
DISSANG <dissangfrancis@yahoo.com>
2023-05-13 23:17:35 +0100

    Add The Logging for the user Event like create_user , update_user, deleted_user

--- Users/User.py
+++ Users/User.py
@@ -1,3 +1,4 @@
+import logging
 from nameko.rpc import rpc
 from nameko.web.handlers import http
 from flask import jsonify
@@ -20,11 +21,23 @@ class UserService:
     name = "user_service"
     db_uri = "postgresql://postgres:kevin@localhost/microservice"
     db = DatabaseSession(Bd)
-
+    def __init__(self):
+            engine = create_engine(self.db_uri)
+            Bd.metadata.create_all(engine)
+            self.db.bind = engine
+      # Create logger
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s [%(name)s] [%(levelname)s] %(message)s')
+    handler = logging.FileHandler('logger.log')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)        
+        
     # ...
 ##ALL THE METHOD RPC OFF THE USER_SERVICE
     @rpc
     def get_all_users(self):
+        self.logger.info('Get all users')
         users = self.db.query(User).all()
         user_list = []
         for user in users:
@@ -34,15 +47,19 @@ class UserService:
                 "email": user.email
             })
         return user_list
+    
     @rpc
     def create_user(self, name, email, password):
+        self.logger.info(f'Create user: {name}')
         new_user = User(name=name, email=email, password=password)
         self.db.add(new_user)
         self.db.commit()
         user_list={"id": new_user.id, "name": new_user.name, "email": new_user.email}
         return user_list
+    
     @rpc
     def update_user(self, user_id, name=None, email=None, password=None):
+        self.logger.info(f'Update user: {user_id}')
         user = self.db.query(User).filter_by(id=user_id).first()
 
         if not user:
@@ -60,16 +77,20 @@ class UserService:
         self.db.commit()
 
         return {'id': user.id, 'name': user.name, 'email': user.email}
+    
     @rpc
     def delete_user(self, user_id):
+        self.logger.info(f'Delete user: {user_id}')
         user = self.db.query(User).get(user_id)
         if user is None:
             return {"error": "User not found"}
         self.db.delete(user)
         self.db.commit()
         return {"message": "User deleted successfully"}
+    
     @rpc
     def get_user_by_id(self, user_id):
+        self.logger.info(f'Get user by id: {user_id}')
         user = self.db.query(User).get(user_id)
         if user is None:
             return {"error": "User not found"}
@@ -78,7 +99,6 @@ class UserService:
             "name": user.name,
             "email": user.email
         }
-     
 ##API CAN BE EXPOSE DIRECTLY WITH NAMEKO     
 
     @http("GET", "/users")
